327bcab41aba32b76663b9a25056a3891b98717f
marcos-ribas25 <marcosvitor530@gmail.com>
2022-06-01 23:23:27 -0300

    feat: responsivo e simulador interna

--- scripts/main.js
+++ scripts/main.js
@@ -13,52 +13,20 @@ $(document).ready(function () {
       BoxMenu.classList.remove('ativo');
    };
 
-   $('.owl-banner').owlCarousel({
-      loop: true,
-      margin: 0,
-      nav: false,
-      items: 1,
-   })
+   const list = document.querySelector('.dropbtn')
+   const listar = document.querySelector('.dropdown-filtrar')
 
-   $('.owl-tintas').owlCarousel({
-      loop: true,
-      margin: 20,
-      nav: false,
-      items: 2,
-      responsive: {
-         0: {
-            items: 1,
-            margin: 0,
-         },
-         600: {
-            items: 2,
-            margin: 15,
-         }
-      }
-   })
+   list.onclick = function () {
+      listar.classList.toggle('ativo');
+   };
 
-   $('.owl-dicas').owlCarousel({
-      loop: true,
-      margin: 20,
-      nav: false,
-      items: 2,
-      responsive: {
-         0: {
-            items: 1,
-         },
-         769: {
-            items: 2,
-            margin: 15,
-         }
-      }
-   })
+   const listInterna = document.querySelector('.dropdown-interno')
 
-   $('.owl-produtos').owlCarousel({
-      loop: true,
-      margin: 20,
-      nav: false,
-      items: 1,
-   })
+   listInterna.onclick = function () {
+      listInterna.classList.toggle('ativo');
+   };
+
+
 
    $("#myInput").on("keyup", function () {
       var value = $(this).val().toLowerCase();
@@ -90,4 +58,82 @@ $(document).ready(function () {
    if ($('.phone-mask').length > 0) {
       $('.phone-mask').mask(SPMaskBehavior, spOptions);
    }
+
+ 
 });
+
+$('.owl-banner').owlCarousel({
+   loop: true,
+   margin: 0,
+   nav: false,
+   items: 1,
+})
+
+$('.owl-tintas').owlCarousel({
+   loop: true,
+   margin: 20,
+   nav: false,
+   items: 2,
+   responsive: {
+      0: {
+         items: 1,
+         margin: 0,
+      },
+      600: {
+         items: 2,
+         margin: 15,
+      }
+   }
+})
+
+$('.owl-dicas').owlCarousel({
+   loop: true,
+   margin: 20,
+   nav: false,
+   items: 2,
+   responsive: {
+      0: {
+         items: 1,
+      },
+      769: {
+         items: 2,
+         margin: 15,
+      }
+   }
+})
+
+$('.owl-produtos').owlCarousel({
+   loop: true,
+   margin: 20,
+   nav: false,
+   items: 1,
+})
+
+let objetoQueSeraPintado = "";
+let tab = "";
+
+function handleColor(color) {
+   document
+      .querySelector(`.${objetoQueSeraPintado}`)
+      .style.setProperty("fill", color, "important");
+
+   document.querySelector("#corSelecionada .text").innerHTML = color
+   document.querySelector("#corSelecionada .ball-color").style.backgroundColor = color
+}
+
+function handleColorTab(tabSelecionado) {
+   const tabs = document.getElementsByClassName("tab-content");
+
+   for (const tab of tabs) {
+      tab.style.display = "none";
+   }
+
+   document.getElementById(tabSelecionado).style.display = "block";
+}
+
+function handle_click_event(obj) {
+   document.querySelector("#colors").style.display = "block";
+   objetoQueSeraPintado = obj.classList[0];
+
+   document.querySelector("#objetoSelecionado").innerHTML = obj.classList[0]
+}
